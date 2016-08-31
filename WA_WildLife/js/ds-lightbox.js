398562be73f064a119cb6237083b851e6370ab9c
kschaudhari <chaudhari.kuladip@gmail.com>
2016-08-31 14:32:30 +0530

    Made changes for lightbox

---
+++ WA_WildLife/js/ds-lightbox.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+
+    // popup show Hide
+    $("#ShwoPopUp").click(function () {
+        var vid = $("#myVideo");
+        $("#mydiv").fadeIn("slow");
+    });
+    $(".CloseBtn").click(function () {
+        $("#mydiv").fadeOut("slow");
+    });
+    // popup show Hide end 
+    $('.MobMenuBtn').off("click");
+    $('.MobMenuBtn').click(function (e) {
+        e.stopPropagation();
+        var content = $(".mobMenuContainer");
+        if ($(content).is(":visible")) {
+            $(content).slideUp("slow");
+        } else {
+            $(content).slideDown("slow");
+        }
+    });
+
+    $(".lightbox-defaultTab .lighbox-boxedMenu .lightbox-menuItem-boxed").hover(function () {
+        $(".lightbox-defaultTab").addClass($(this).attr("data-tab") + "BG");
+        $(this).addClass($(this).attr("data-tab") + "Hover");
+        $(this).addClass("hover");
+    }, function () {
+        $(".lightbox-defaultTab").removeClass($(this).attr("data-tab") + "BG");
+        $(this).removeClass($(this).attr("data-tab") + "Hover");
+        $(this).removeClass("hover");
+    })
+
+});
