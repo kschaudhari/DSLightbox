60467325b32a32cc2766a36adf31f57c92e14781
kschaudhari <chaudhari.kuladip@gmail.com>
2016-09-29 10:30:39 +0530

    exception handling

--- BA_Bermuda/js/ds-lightbox.js
+++ BA_Bermuda/js/ds-lightbox.js
@@ -3,10 +3,11 @@
 }
 $(document).ready(function () {
     var type = window.location.hash.substr(1);
-    var is_mrs_reffer = document.referrer.indexOf("mrandmrssmith.com") > -1;
-    if (is_mrs_reffer)
-    {
-        $(".lighbox-container").addClass("smithSource");
+    if (document.referrer) {
+        var is_mrs_reffer = document.referrer.indexOf("mrandmrssmith.com") > -1;
+        if (is_mrs_reffer) {
+            $(".lighbox-container").addClass("smithSource");
+        }
     }
 
     // popup show Hide
