3f1a1486d109c1c29e638baeb0318e8398b60bbb
kschaudhari <chaudhari.kuladip@gmail.com>
2016-10-14 15:55:40 +0530

    made changes for woopra

--- WA_Qatar/js/creative-util.js
+++ WA_Qatar/js/creative-util.js
@@ -37,7 +37,18 @@ function sendTrackerInfo() {
         if (typeof ga !== "undefined")
             ga('send', 'pageview');
         //add any pixel info to track
-        (function (d, p) { var a = new Image(); a.onload = function () { a = null }; a.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//rs.gwallet.com/r1/pixel/x" + p + "r" + Math.round(1E9 * Math.random()) })(document, "40229")
+        // radium one
+        (function (d, p) { var a = new Image(); a.onload = function () { a = null }; a.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//rs.gwallet.com/r1/pixel/x" + p + "r" + Math.round(1E9 * Math.random()) })(document, "40229");
+        // woopra pixel
+        (function () {
+            var t, i, e, n = window, o = document, a = arguments, s = "script", r = ["config", "track", "identify", "visit", "push", "call", "trackForm", "trackClick"], c = function () { var t, i = this; for (i._e = [], t = 0; r.length > t; t++) (function (t) { i[t] = function () { return i._e.push([t].concat(Array.prototype.slice.call(arguments, 0))), i } })(r[t]) }; for (n._w = n._w || {}, t = 0; a.length > t; t++) n._w[a[t]] = n[a[t]] = n[a[t]] || new c; i = o.createElement(s), i.async = 1, i.src = "//static.woopra.com/js/w.js", e = o.getElementsByTagName(s)[0], e.parentNode.insertBefore(i, e)
+        })("woopra");
+
+        woopra.config({
+            domain: 'waqatar.dslabs.io'
+        });
+        woopra.track();
+
         return;
     }
     loadedTimerForTracker = window.setTimeout(sendTrackerInfo, 500);
