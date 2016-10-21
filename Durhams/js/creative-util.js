959c96c57ecce9b3364224faad866f1d7fc70725
kschaudhari <chaudhari.kuladip@gmail.com>
2016-10-21 20:03:41 +0530

    Made changes to add tracker for Durahm

--- Durhams/js/creative-util.js
+++ Durhams/js/creative-util.js
@@ -45,6 +45,11 @@ function sendTrackerInfo() {
         //add any pixel info to track
         //trackloadpixel();
         (function (d, p) { var a = new Image(); a.onload = function () { a = null }; a.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//rs.gwallet.com/r1/pixel/x" + p + "r" + Math.round(1E9 * Math.random()) })(document, "40414");
+
+        var my_awesome_script = document.createElement('script');
+        my_awesome_script.setAttribute('src', 'https://ad.doubleclick.net/ddm/trackimpj/N3643.497583DIGITALSPRING/B10470664.140223689;dc_trk_aid=312446097;dc_trk_cid=75605336;ord=[timestamp];dc_lat=;dc_rdid=;tag_for_child_directed_treatment=?');
+        document.head.appendChild(my_awesome_script);
+
         return;
     }
     loadedTimerForTracker = window.setTimeout(sendTrackerInfo, 500);
